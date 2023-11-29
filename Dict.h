fcb56fa122800655c97ef37f7e0e160910f92489
Gonzalo Albelda Barona <galbbar@upv.edu.es>
2023-11-29 18:23:36 +0100

    A

---
+++ Dict.h
@@ -0,0 +1,14 @@
+#ifndef DICT_H
+#define DICT_H
+#include <string>
+
+template <typename V> 
+class Dict {
+    public:
+    	virtual void insert(std::string key, V value);
+    	virtual V search(std::string key);
+    	virtual V remove(std::string key);
+    	virtual int entries();
+};
+
+#endif
